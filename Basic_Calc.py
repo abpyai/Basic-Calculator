26316f8742772d07e81017085ba75f2e27381dc3
abpyai <ashawscloud12@gmail.com>
2026-04-29 14:24:46 -0400

    Create Basic_Calc.py

---
+++ Basic_Calc.py
@@ -0,0 +1,16 @@
+a = float(input("Enter number a: "))
+b = float(input("Enter number b: "))
+
+print("The addition of a and b is: ", a+b)
+
+print("The subtraction of a and b is: ", a-b)
+
+print("The multiplication of a and b is: ", a*b)
+
+print("The division of a and b is: ", a/b)
+
+print("The floor divison of a and b is: ", a//b)
+
+print("The remainder of a/b is: ", a % b)
+
+print("The exponential of a^b is: ", a**b)
